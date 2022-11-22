46e78e2db836a59d3a6576949dc0cb86e868d7ed
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-11-23 00:26:59 +0100

    feat: learn types of print

--- src/print.rs
+++ src/print.rs
@@ -1,3 +1,25 @@
 pub fn run() {
-    println!("Hello from the print.rs file")
+    // Print to console
+    println!("Hello from the print.rs file");
+
+    // Basic Formatting
+    println!("Number: {}", 1);
+
+    // Positional Arguments
+    println!(
+        "{0} is from {1} and {0} likes to {2}",
+        "AG", "Mass", "code"
+    );
+
+    // Named Arguments
+    println!(
+        "{name} likes to play {activity}",
+        name="António Gabriel", activity = "piano"
+    );
+
+    // Placeholder traits
+    println!("Binary: {:b} Hex: {:x} Octal: {:0}", 10, 10, 10);
+
+    // Placeholder for debug trait
+    println!("{:?}", (12, 23, 23));
 }
