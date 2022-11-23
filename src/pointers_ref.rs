88e7eecefb20b0c90105bb1a59347b0d4bcef205
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-11-23 17:41:25 +0100

    feat: pointers ref

---
+++ src/pointers_ref.rs
@@ -0,0 +1,14 @@
+// Reference Pointers - Point to a resource in memory
+
+pub fn _run() {
+    // Primitive Array
+    let arr1 = [1, 2, 3];
+    let arr2 = arr1;
+
+    println!("Values: {:?}", (arr1, arr2));
+
+    let vec1 = vec![1, 2, 3];
+    let vec2 = &vec1;
+
+    println!("Values: {:?}", (&vec1, vec2));
+}
