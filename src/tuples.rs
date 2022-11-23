570fb2c17b3b37ab942ec0f80207d4245acd7016
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-11-23 07:07:37 +0100

    feat: tuples

---
+++ src/tuples.rs
@@ -0,0 +1,7 @@
+// Tuples group together values of different types
+// Max 12 elements 
+
+pub fn _run() {
+    let person: (&str, &str, i8) = ("António", "Gabriel", 26);
+    println!("{:?}", person);
+}
